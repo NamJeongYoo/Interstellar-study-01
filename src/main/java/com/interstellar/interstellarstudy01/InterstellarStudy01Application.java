4ebbe335fc277cff2c3fc1c3bb8d623bd1f97e74
namjeong-yoo <skawjddb@gmail.com>
2026-02-07 13:27:34 +0900

    interstellar study first commit

---
+++ src/main/java/com/interstellar/interstellarstudy01/InterstellarStudy01Application.java
@@ -0,0 +1,13 @@
+package com.interstellar.interstellarstudy01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterstellarStudy01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterstellarStudy01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/interstellar/interstellarstudy01/InterstellarStudy01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interstellar.interstellarstudy01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterstellarStudy01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
